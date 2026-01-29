dea5f4573029ec831748bc65da1e573ad2d0737d
Pedro Lemos <pedrotlgl@gmail.com>
2026-01-29 14:09:18 -0300

    chore: cria estrutura inicial do projeto maven com java 17

---
+++ 01-integracao-api/integracao-ans/src/main/java/br/com/intuitivecare/Main.java
@@ -0,0 +1,8 @@
+package main.java.br.com.intuitivecare;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Projeto Integração ANS iniciado");
+    }
+    
+}
